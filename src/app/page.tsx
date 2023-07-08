63b371f4e9cd57fd18afcad4d5f730c80ad86687
akiko_kuroda <hahnecochito@gmail.com>
2023-07-09 04:03:29 +0900

    🐛

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
 import Image from 'next/image'
 
 
-export const ReadAllItems = () => {
-  return (
-    <div>
-      <h1 className='h1-style'>こんにちわ</h1>
-    </div>
-  )
-}
-
 export default function Home() {
   return (
     <main>
       index
-      <ReadAllItems />
     </main>
   )
 }
