e0c038523d347876538d0a2efeab28d369a796fc
akiko_kuroda <hahnecochito@gmail.com>
2023-07-08 15:45:18 +0900

    fix

--- utils/database.tsx
+++ utils/database.tsx
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async() => {
   try { // したい処理
-    await mongoose.connect("mongodb+srv://hahnecochito:S5haJdAot3cbbIsp@cluster0.c0xxupd.mongodb.net/?retryWrites=true&w=majority")
+    await mongoose.connect("mongodb+srv://hahnecochito:S5haJdAot3cbbIsp@cluster0.c0xxupd.mongodb.net/appDataBase?retryWrites=true&w=majority")
     console.log("Success: Connected to MongoDB")
   } catch (err) { // 失敗した時!!
     console.log("Failure: Unconnected to MongoDB")
